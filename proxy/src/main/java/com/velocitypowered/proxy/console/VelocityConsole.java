2b5d964ab014dac0bed6ae6ff7b741b43731bdfd
Shane Freeder <theboyetronic@gmail.com>
2026-07-11 01:36:17 +0100

    Reapply "Adventure 5 (#1774)"
    This reverts commit 06eb052ab507dab1ee96787f7363fc34d8adfce3.

--- proxy/src/main/java/com/velocitypowered/proxy/console/VelocityConsole.java
+++ proxy/src/main/java/com/velocitypowered/proxy/console/VelocityConsole.java
@@ -28,7 +28,6 @@ import com.velocitypowered.proxy.util.ClosestLocaleMatcher;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Locale;
-import net.kyori.adventure.audience.MessageType;
 import net.kyori.adventure.identity.Identity;
 import net.kyori.adventure.permission.PermissionChecker;
 import net.kyori.adventure.platform.facet.FacetPointers;
@@ -73,8 +72,7 @@ public final class VelocityConsole extends SimpleTerminalConsole implements Cons
   }
 
   @Override
-  public void sendMessage(@NonNull Identity identity, @NonNull Component message,
-      @NonNull MessageType messageType) {
+  public void sendMessage(@NonNull Component message) {
     componentLogger.info(message);
   }
 
